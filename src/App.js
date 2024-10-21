da7773ed30799f1ee52f11abe256f877b41dc31e
sivasaigiri <sivasaigiri8897@gmail.com>
2024-10-21 22:36:42 +0530

    components created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
 import React from 'react';
 import './App.css';
 
+import React from 'react';
+import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+
 function App() {
   return (
-    <div className="App" style={{ backgroundColor: 'white', height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-      <h1>Hello, this is my first React project</h1>
-    </div>
+    <Router>
+      <div>
+        {/* Navigation Bar */}
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* Route Definitions */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
 export default App;
+
---
+++ src/components/Internships.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Internships() {
+  return (
+    <div>
+      <h2>About Page</h2>
+      <p>This is the about page.</p>
+    </div>
+  );
+}
+
+export default Internships;
+
