e54e9a06433765f75d39f6a5e7bb65c232abddd4
sivasaigiri <sivasaigiri8897@gmail.com>
2025-01-12 22:48:18 +0530

    update

--- src/App.js
+++ src/App.js
@@ -51,7 +51,7 @@ function App() {
 
       <section style={{ padding: "20px", textAlign: "center" }}>
         <h2>Contact Me</h2>
-        <p>Email: <a href="mailto:your-email@example.com">your-email@example.com</a></p>
+        <p>Email: <a href="mailto:your-email@example.com">your-email@example</a></p>
         <p>LinkedIn: <a href="https://linkedin.com/in/your-profile" target="_blank" rel="noopener noreferrer">your-profile</a></p>
       </section>
 
